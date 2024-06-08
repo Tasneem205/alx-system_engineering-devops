85227eff43a0adc2f52856261c3e253ff61adb2a
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-06-08 23:15:46 +0300

    try task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,9 +7,10 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "MyRedditApp/0.1 (by tasneemabdeltawab205@gmail.com)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
+    response.raise_for_status()
     if response.status_code == 404:
         return 0
     results = response.json().get("data")

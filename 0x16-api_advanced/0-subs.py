b38e2afe78dad800dcd2288f5ca31a3d8db444e4
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-06-08 20:42:24 +0300

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,6 @@ def number_of_subscribers(subreddit):
     """ return the number of sunscribbers to a subreddit """
     headers = {'User-Agent': 'Mozilla/5.0 (compatible; CustomBot/0.1)'}
     if subreddit is None or type(subreddit) is not str:
-        print("subreddit is none or not a string")
         return (0)
     r = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
                      headers=headers)
@@ -18,5 +17,4 @@ def number_of_subscribers(subreddit):
     if r:
         return data['data']['subscribers']
     else:
-        print("request is not defined")
         return (0)

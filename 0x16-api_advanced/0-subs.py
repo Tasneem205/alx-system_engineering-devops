51d75b88e2e55e0f65f0150433eda38b83d9b281
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-06-08 20:40:44 +0300

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,16 +2,18 @@
 """
 number of followers to a subreddit
 """
-import requests
 import json
+import requests
+
 
 def number_of_subscribers(subreddit):
     """ return the number of sunscribbers to a subreddit """
     headers = {'User-Agent': 'Mozilla/5.0 (compatible; CustomBot/0.1)'}
-    if subreddit is None or type(subreddit) is  not str:
+    if subreddit is None or type(subreddit) is not str:
         print("subreddit is none or not a string")
         return (0)
-    r = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json", headers=headers)
+    r = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
+                     headers=headers)
     data = r.json()
     if r:
         return data['data']['subscribers']
